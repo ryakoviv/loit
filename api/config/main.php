c758324923bc8c53acdcb2ef6dd0a008b3f16bc2
roman.yakoviv <r.yakoviv@gmail.com>
2018-12-30 18:26:36 +0200

    Add API

---
+++ api/config/main.php
@@ -0,0 +1,70 @@
+<?php
+
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/params.php')
+);
+
+return [
+    'id' => 'app-api',
+    'basePath' => dirname(__DIR__),    
+    'bootstrap' => ['log'],
+    'modules' => [
+        'v1' => [
+            'basePath' => '@app/modules/v1',
+            'class' => 'api\modules\v1\Module'
+        ]
+    ],
+    'components' => [
+        'response' => [
+            'format' => yii\web\Response::FORMAT_JSON,
+            'charset' => 'UTF-8',
+        ],
+        'request' => [
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ],
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => env('API_COOKIE_VALIDATION_KEY'),
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => false,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'enableStrictParsing' => true,
+            'showScriptName' => false,
+            'rules' => [
+//                [
+//                    'class' => 'yii\rest\UrlRule',
+//                    'controller' => 'v1/feature',
+//                    'extraPatterns' => [
+//                        'OPTIONS <action:\w+>' => 'options'
+//                    ]
+//                ],
+//                [
+//                    'class' => 'yii\rest\UrlRule',
+//                    'controller' => 'v1/user',
+//                    'extraPatterns' => [
+//                        'POST login' => 'login',
+//                        'OPTIONS <action:\w+>' => 'options'
+//                    ]
+//                ],
+            ],
+        ]
+    ],
+    'params' => $params,
+];
+
+
+
